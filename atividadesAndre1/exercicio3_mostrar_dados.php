5d9c89c7b0b30df933882c3c159b6ba08f7aa914
Andre Luis Correa <55238564+Andre-Luis-Correa@users.noreply.github.com>
2021-05-04 17:05:41 -0300

    Add files via upload

---
+++ atividadesAndre1/exercicio3_mostrar_dados.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercício 3</title>
+</head>
+
+<body>
+
+    <h2> A frase gerada foi: <?= $frase_fake ?> </h2>
+
+    <p>Quer brincar mais? Clique no botão abaixo:</p>
+
+    <a href="exercicio3_index.php">
+        <button> CLIQUE AQUI </button>
+    </a>
+
+
+</body>
+
+</html>
